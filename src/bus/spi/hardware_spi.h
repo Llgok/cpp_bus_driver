aac94df63e0414d457d1a0d6e9415a82f072850a
LILYGO_L <13073925292@163.com>
2025-08-08 14:02:43 +0800

    reduce the access level of the Hardware_Spi variable to public

--- src/bus/spi/hardware_spi.h
+++ src/bus/spi/hardware_spi.h
@@ -2,7 +2,7 @@
  * @Description: None
  * @Author: LILYGO_L
  * @Date: 2024-12-16 17:47:28
- * @LastEditTime: 2025-06-18 14:19:29
+ * @LastEditTime: 2025-08-08 14:01:01
  * @License: GPL 3.0
  */
 #pragma once
@@ -14,7 +14,7 @@ namespace Cpp_Bus_Driver
 #if defined DEVELOPMENT_FRAMEWORK_ESPIDF
     class Hardware_Spi : public Bus_Spi_Guide
     {
-    private:
+    public:
         int32_t _mosi, _sclk, _miso, _cs, _freq_hz;
         spi_host_device_t _port;
         uint8_t _mode;
@@ -22,7 +22,6 @@ namespace Cpp_Bus_Driver
 
         spi_device_handle_t _spi_device;
 
-    public:
         Hardware_Spi(int32_t mosi, int32_t sclk, int32_t miso = DEFAULT_CPP_BUS_DRIVER_VALUE,
                      spi_host_device_t port = SPI2_HOST, int8_t mode = 0, uint32_t flags = DEFAULT_CPP_BUS_DRIVER_VALUE)
             : _mosi(mosi), _sclk(sclk), _miso(miso), _port(port), _mode(mode), _flags(flags)
