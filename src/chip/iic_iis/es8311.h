df1125e66916add879895d0a790f14a6ef0b735c
LILYGO_L <13073925292@163.com>
2025-09-03 10:16:44 +0800

    update es8311 channel settings

--- src/chip/iic_iis/es8311.h
+++ src/chip/iic_iis/es8311.h
@@ -2,7 +2,7 @@
  * @Description: None
  * @Author: LILYGO_L
  * @Date: 2025-03-11 16:42:57
- * @LastEditTime: 2025-09-02 17:14:00
+ * @LastEditTime: 2025-09-03 10:15:44
  * @License: GPL 3.0
  */
 
@@ -322,7 +322,8 @@ namespace Cpp_Bus_Driver
         bool begin(i2s_mclk_multiple_t mclk_multiple, uint32_t sample_rate_hz, i2s_data_bit_width_t data_bit_width) override;
 
 #elif defined DEVELOPMENT_FRAMEWORK_ARDUINO_NRF
-        bool begin(nrf_i2s_ratio_t mclk_multiple, uint32_t sample_rate_hz, nrf_i2s_swidth_t data_bit_width, nrf_i2s_channels_t channel) override;
+        bool begin(nrf_i2s_ratio_t mclk_multiple, uint32_t sample_rate_hz, nrf_i2s_swidth_t data_bit_width,
+                   nrf_i2s_channels_t channel = nrf_i2s_channels_t::NRF_I2S_CHANNELS_STEREO) override;
 #endif
 
         uint16_t get_device_id(void);
